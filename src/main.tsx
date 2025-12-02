1c6bb1f90817ef1d3d09db30bdb5002de9195b4d
miso <misomiso0307@gmail.com>
2025-12-03 02:44:36 +0900

    feat: 전역 영화 위시리스트 기능 구현

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./styles/global.css";
+import { AuthProvider } from "./context/AuthContext";
+import {WishlistProvider} from "./context/WishlistContext.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <AuthProvider>
+            <WishlistProvider>
+                <App />
+            </WishlistProvider>
+        </AuthProvider>
+    </BrowserRouter>
+);
