39080f513ab08573b2dda6f1177457a47fbd29df
miso <misomiso0307@gmail.com>
2025-12-11 21:17:29 +0900

    fix: HashRouter 적용 및 preview 404 오류 해결

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./styles/global.css";
 import { AuthProvider } from "./context/AuthContext";
 import {WishlistProvider} from "./context/WishlistContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
         <AuthProvider>
             <WishlistProvider>
                 <App />
             </WishlistProvider>
         </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
 );
